5aafa9d37c38ffd5e3321176d7235eae7ec16ed3
PVigar88 <Pvigar@gmail.com>
2021-04-03 10:58:37 -0400

    added API pull, state and Character Function

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,7 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+export default function Character({ character }) {
+  return <div>character</div>;
+}
